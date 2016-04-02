7ed150f3b1e08b34e57a9048bc85786dad15ea29
Jakukyo Friel <weakish@gmail.com>
2016-04-02 22:04:18 +0800

    Check PostgreSQL store schema version.

--- shatag/store/pg.py
+++ shatag/store/pg.py
@@ -5,6 +5,9 @@
 import shatag
 import psycopg2
 
+from shatag.base import SQLDatabaseIncompatibleError
+
+
 class PgStore(shatag.base.SQLStore):
     """A postgresql store using psycopg2."""
     def __init__(self, url=None, name=None):
@@ -20,8 +23,37 @@ class PgStore(shatag.base.SQLStore):
             cursor.execute(
                 'create table contents(hash varchar(64), size bigint, name varchar(50), path varchar(100), primary key (name, path))')
             cursor.execute('create index content_hash on contents(hash)')
+            cursor.execute('create table version(user_version bigint)')
+            cursor.execute('insert into version(user_version) values(1)')
         except psycopg2.ProgrammingError:
+            # Check database version.
+            try:
+                cursor.execute('select user_version from version')
+                user_version = cursor.fetchone()[0]
+                if user_version != 1:
+                    message = """
+It seems that you are using an old version of PostgreSQL store. New versions of
+shatag changed PostgreSQL database schema.
+
+You have to clean up the PostgreSQL database mentioned below, and record all
+tags again (e.g. `-pqr`). Alternatively, you can use a new PostgreSQL store,
+via using a command line option (`-d`) or editing configuration file
+(`~/.shatagrc`).
+
+Since we added new meta information (size) of files, we cannot upgrade the
+sqlite store automatically. You have to start over.
+                    """
+                    raise SQLDatabaseIncompatibleError(url, message)
+            except SQLDatabaseIncompatibleError as e:
+                print(e.message)
+                print('Incompatible PostgreSQL store:', e.url)
+                import sys
+                # As defined in `sysexits.h`:
+                #       #define EX_DATAERR    65    /* data format error */
+                sys.exit(65)
+
             db.rollback()
+
 
     # reimplementing these because psycopg2 does not handle classic placeholders correctly
     def clear(self, base='/',name=None):
